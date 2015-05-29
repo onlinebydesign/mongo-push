9828d5bfbf67be36625a105a093f3ffc8c9570f8
Jedediah Smith <jed@obdstudios.com>
2015-05-29 14:57:41 -0600

    Created package.json and created the first test.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,55 @@
-var mongoPush = require('mongo-push')();
+var mongoPush = new require('mongo-push')();
+var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-var doSync = function (cb) {
-    mongoPush({
-        src: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]',
-        dst: 'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]'
-    }, function (err) {
-        if (err) return err;
-    });
+var src = 'mongodb://localhost/mongoPushTest1';
+var dst = 'mongodb://localhost/mongoPushTest2';
 
-};
+// To run the test we need to connect to the mongo databases
+MongoClient.connect(src, function(err, srcDb) {
+    assert.equal(err, null);
+    console.log("Connected to source database");
+    var srcCol = srcDb.collection('test');
+    MongoClient.connect(src, function(err, dstDb) {
+        assert.equal(err, null);
+        console.log("Connected to destination database");
+        var dstCol = dstDb.collection('test');
+
+        var doSync = function (cb) {
+            mongoPush.sync({
+                src: src,
+                dst: dst
+            }, function (err) {
+                if (err) return cb(err);
+
+                srcCol.find().toArray(function (err, srcData) {
+                    if (err) return cb(err);
+
+                    dstCol.find().toArray(function (err, dstData) {
+                        if (err) return cb(err);
+
+                        // Return the data for both the source and the destination database
+                        return cb(null, {srcData: srcData, dstData: dstData});
+                    });
+                });
+            });
+        };
+
+
+        /**
+         *  Start the assertions
+         */
+
+        // If unable to connect to either database it should return an error
+        mongoPush.sync({
+            src: src,
+            dst: dst
+        }, function (err, results) {
+            assert.ok(err, 'Should return an error');
+            assert.ok(!results, 'Should not return any results')
+        })
+    });
+});
+
 
 
